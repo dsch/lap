c42341e8c56a697de74257b290c43c2717c96e93
David Schneider <schneidav81@gmail.com>
2018-12-29 21:55:58 +0100

    simple implementation

---
+++ proxy_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func listen(server *http.Server) {
+	log.Fatal(server.ListenAndServe())
+}
+
+func TestGet(t *testing.T) {
+	proxy, client := startProxy()
+	defer proxy.Close()
+
+	server := startHttpServer()
+	defer server.Close()
+
+	t.Run("Get", func(t *testing.T) {
+		resp, err := client.Get(server.URL)
+		assert.Equal(t, nil, err, "no error")
+		assert.Equal(t, http.StatusOK, resp.StatusCode, "status OK")
+		body, err := ioutil.ReadAll(resp.Body)
+		assert.Equal(t, []byte("OK"), body)
+	})
+
+	t.Run("Head", func(t *testing.T) {
+		resp, err := client.Head(server.URL)
+		assert.Equal(t, nil, err, "no error")
+		assert.Equal(t, http.StatusOK, resp.StatusCode, "status OK")
+	})
+}
+
+func startHttpServer() *httptest.Server {
+	// Start a local HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte(`OK`))
+	}))
+	return server
+}
+
+func startProxy() (*http.Server, *http.Client) {
+	proxy := &http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(Handler),
+	}
+	go listen(proxy)
+
+	proxyUrl := &url.URL{Scheme: "http", Host: "localhost:8080"}
+	client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+
+	return proxy, client
+}
